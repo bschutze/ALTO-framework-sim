b1f4dca246707d7cb9db531765f4a27962c9c31d
Johannes Schuetze <jschuetze@benocs.com>
2014-04-25 15:24:34 +0200

    started implementing tracerout. ping_.py is not yet used but soon will flow into structure

--- Views/Traceroute_.py
+++ Views/Traceroute_.py
@@ -15,4 +15,26 @@
 #	interfaces and delay to the interface
 #	
 
+from libraries.dijkstra import *
 
+
+#********************************************************************
+########################### PROGRAM START ###########################
+#********************************************************************
+
+#0-100 The percentage of vantage points
+vantagePercentage  = str(sys.argv[1])
+#0-100 The percentage of network nodes hidden
+hiddenPercentage = int(sys.argv[2])
+#a dictionary in the format fit for djikstra algorithm and shortest path
+neighborHood = str(sys.argv[3])
+
+
+#find total number of nodes.
+#calculate % hidden
+#determin what nodes are interior and what nodes are exterior
+#use seeded random to decide what nodes are hidden
+#use same seed for determining the vantage points (ensures reproducibility)
+#find shortest path from all vantage points to all nodes.
+#read out and store the headlabels from the shortest "return" path (back to vantage point)
+#pass data to rocketfuel script or create "traceroute" view of the network.
