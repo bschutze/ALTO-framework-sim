80734e16506e1c30c664c6d6d76444af95bc8c0b
Johannes Schuetze <jschuetze@benocs.com>
2014-05-08 14:56:10 +0200

    some commit stuff I had left to do at work. (Abstarct extended)

--- ping_.py
+++ ping_.py
@@ -4,11 +4,18 @@
 #Date: 14.01.2014
 #Author: Bruno-Johannes Schuetze
 #uses python 2.7.6
+#uses the djikstra algorithm implemented by David Eppstein
 
-#Module does calculations to behave similar to ping
+#Module does calculations to behave similar to ping, uses delay label defined in the dot file
+from libraries.dijkstra import *
 
-def ping(src, dst, edgeCostHash):
+
+def getSingleValue(src, dst, edgeCostHash):
 	return edgeCostHash[(src*100000)+dst]
 
-def doThePing(edgeCostHash):
-	
+def getPathTotal(start, end, edgeCostHash, networkDict):
+	#get shortest path between start and end
+	shortPathList = shortestPath(networkDict, start, end)
+	print "WE PINGING SHAWTY", shortPathList
+
+
