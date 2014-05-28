e62039536c570ab2b25a6464a9475224d2cdd3f1
Johannes Schuetze <jschuetze@benocs.com>
2014-05-28 11:52:39 +0200

    cleaned up a little bit and added a utilities folder with important scripts

---
+++ utilities/drawGraph_.py
@@ -0,0 +1,27 @@
+#!/local/bin/python
+
+#Master-Thesis dot parsing framework (PING MODULE)
+#Date: 14.01.2014
+#Author: Bruno-Johannes Schuetze
+#uses python 2.7.6
+
+#this module takes a neighborhoodDict and generates a graph out of it
+
+import pydot
+from PIL import Image
+#function transforms
+#def transformGraphReady():
+
+def drawGraph(hoodDict, grName):
+	graph = pydot.Dot(label = grName, graph_type='digraph')
+	graph.set_edge_defaults(len='2')
+	#first we generate the nodes
+	for key in hoodDict.keys():
+		node_a = pydot.Node(str(key), shape="box")
+		graph.add_node(node_a)
+	for node in hoodDict.keys():
+		for entry, value in hoodDict[node].items():
+			edge = pydot.Edge(str(node), str(entry), label = str(value), )
+			graph.add_edge(edge)
+	graph.write_jpeg('Output/ALTO_AGG_NET.jpeg', prog='neato')
+	Image.open('Output/ALTO_AGG_NET.jpeg').show()
